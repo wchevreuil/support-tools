a0a60866c3824ed2415ccc13cceeb8a97048dbaf
wellington <wchevreuil@cloudera.com>
2017-04-11 10:59:01 +0100

    added javadocs comments

--- src/main/java/com/cloudera/support/hbase/ArchiveFileSnapshotReference.java
+++ src/main/java/com/cloudera/support/hbase/ArchiveFileSnapshotReference.java
@@ -10,6 +10,36 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.master.snapshot.SnapshotHFileCleaner;
 
+/**
+ * This program allows to inspect hfiles on specified path,
+ * checking if the given hfiles are still referenced by any existing snapshot.
+ * 
+ * The main goal is to be used as an alternative tool to HBase's own 
+ * archive cleaning feature, mainly on situations where archive folder
+ * has grown too large, and HMaster's cleaner thread is taking too long
+ * to reclaim HDFS space, whilst using this tool, it's possible to specify
+ * specific subdirs within archive directory, for example, older dirs or those
+ * related to a recently deleted snapshot, which are more likely to have 
+ * unreferenced files.
+ * 
+ * It traverses the specified path recursively. 
+ * It should be only passed subdirectories of hbase archive folder. 
+ * It currently does not not perform any validation on that, passing paths for 
+ * hbase tables (i.e. "/hbase/data/default/table_name") may lead to 
+ * unpredicted results and has not been tested. Please don't do that.
+ * 
+ * Usage:
+ *
+ * com.cloudera.support.hbase.ArchiveFileSnapshotReference ARCHIVE_SUBDIR [delete]
+ * 
+ * The only required parameter is the archive subdir. In this mode, 
+ * it will go through the informed directry structure, 
+ * checking hfiles that can be deleted. At the end a summary of how much files
+ * would be deleted and hdfs space would be released is printed.
+ * 
+ * Passing optional "delete" flag as second parameter would cause the unreferenced
+ * files to be deleted, releasing hdfs space.
+ */
 public class ArchiveFileSnapshotReference {
 
   private SnapshotHFileCleaner fileCleaner = new SnapshotHFileCleaner();
